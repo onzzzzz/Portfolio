600e1b7533ea23e73b9791eaf35665a220a9f39f
onzzzzz <ninaonz.shop@gmail.com>
2023-06-21 19:01:53 +0100

    g

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,31 +5,31 @@ import CanvasLoader from '../Loader';
 
 const Computers = ({isMobile}) => {
   
-  const computer = useGLTF('./planet/scene.gltf')
+  const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
-    // <mesh>
-    //   <hemisphereLight intensity={0.15} groundColor="purple"/>
-    //   <pointLight intensity={3}/>
-    //   <spotLight
-    //   position={ [ 0 , 10 , -3]}
-    //   angle={0.32} 
-    //   penumbra={1}
-    //   intensity={5}
-    //   castShadow
-    //   shadow-mapSize={1024}
-    //   />
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="purple"/>
+      <pointLight intensity={3}/>
+      <spotLight
+      position={ [ 0 , 10 , -3]}
+      angle={0.32} 
+      penumbra={1}
+      intensity={5}
+      castShadow
+      shadow-mapSize={1024}
+      />
       <primitive 
         object={computer.scene}
-        scale={isMobile ? 0.05 : 0.1}
-        // position={isMobile ? [0, 0, 0] : [0,0,0] }
+        scale={isMobile ? 0.3 : 0.5}
+        position={isMobile ? [0, 0, 0] : [0,0,0] }
         // rotation={[0,-1,-1 ]}
-        position-y={0}
+        // position-y={0}
         rotation-y={-5}
-        rotation-x={0.5}s
+        // rotation-x={0.5}s
         
 
       />
-    // </mesh>
+    </mesh>
   )
 }
 
@@ -55,10 +55,10 @@ const ComputersCanvas = () => {
     shadows
     frameloop="demand"
     camera={{ 
-      fov: 45,
+      fov: 25,
       near: 0.1,
       far: 200,
-      position: [-4, 3, 6]
+      position: [0, 3, 6]
     }}
     // camera={{ positon: [20, -3 , -5], fov: 5 }}
     gl={{preserveDrawingBuffer: true }}
