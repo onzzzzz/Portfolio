07fdc9c766a90ad8abd17f6b83627bebd85031bb
onzzzzz <ninaonz.shop@gmail.com>
2023-06-21 18:08:34 +0100

    Initial commit

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,79 @@
+import { Suspense, useEffect, useState } from "react";
+import { Canvas} from '@react-three/fiber';
+import { OrbitControls , Preload, useGLTF} from "@react-three/drei";
+import CanvasLoader from '../Loader';
+
+const Computers = ({isMobile}) => {
+  
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="purple"/>
+      <pointLight intensity={3}/>
+      <spotLight
+      position={ [ 0 , 10 , -3]}
+      angle={0.32} 
+      penumbra={1}
+      intensity={5}
+      castShadow
+      shadow-mapSize={1024}
+      />
+      <primitive 
+        object={computer.scene}
+        scale={isMobile ? 0.5 : 0.5}
+        position={isMobile ? [0, 0, 0] : [0,0,0] }
+        rotation={[0,-1,-1 ]}
+        
+
+      />
+    </mesh>
+  )
+}
+
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+  }, [])
+  return (
+    <Canvas
+    frameloop="demand"
+    shadows
+    camera={{ positon: [20, -3 , -5], fov: 5 }}
+    gl={{preserveDrawingBuffer: true }}
+    
+    >
+    
+    <Suspense fallback={<CanvasLoader />} >
+      <OrbitControls
+      autoRotate="true" 
+      enableZoom={false}
+      maxPolarAngle={Math.PI / 2 }
+      minPolarAngle={Math.PI / 2}
+      
+
+      />
+
+      <Computers isMobile={isMobile} />
+
+    </Suspense>
+
+    <Preload all />
+
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas;
